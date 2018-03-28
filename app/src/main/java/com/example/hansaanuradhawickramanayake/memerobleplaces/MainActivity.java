16dbde5a2e5178f34fdf16dfec7f78262fea3188
Hansaanuradha93 <hansaanuradha93@gmail.com>
2018-03-28 13:21:51 +0530

    first commit

---
+++ app/src/main/java/com/example/hansaanuradhawickramanayake/memerobleplaces/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.hansaanuradhawickramanayake.memerobleplaces;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addNewPlaces(View view){
+
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+
+        startActivity(intent);
+
+    }
+}
